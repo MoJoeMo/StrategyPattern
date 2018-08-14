d04560a6d9aea15af14820a3172e45c364f3645d
joeyclausen <joeypclausen@gmail.com>
2018-08-13 22:42:21 -0500

    StrategyPattern: Initial attempt and commit on strategy pattern.

--- CalculatorStrategy/Program.cs
+++ CalculatorStrategy/Program.cs
@@ -1,8 +1,11 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
+using CalculatorStrategy.Enum;
+using CalculatorStrategy.Operators;
 
 namespace CalculatorStrategy
 {
@@ -10,8 +13,15 @@ namespace CalculatorStrategy
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
-			Console.ReadKey();
+			List<int> calcs = new List<int>();
+			Calculator _calculator = new Calculator();
+			calcs.Add(_calculator.PerformOperation("Add", 44, 87));
+			calcs.Add(_calculator.PerformOperation("Multiply", 10, 12));
+			calcs.Add(_calculator.PerformOperation("Subtract", 25, 36));
+			calcs.Add(_calculator.PerformOperation("Divide", 100, 10));
+			calcs.ForEach(Console.WriteLine);
+
+			Console.Read();
 		}
 	}
 }
